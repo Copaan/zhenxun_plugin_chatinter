29050443477ea1f66e34ae026e7bff35d0befbb6
meng-luo <admin@mengluo.work>
2026-08-28 10:57:57 +0800

    Update plugin documentation and version to 1.5.0

--- chatinter/plugin_entry.py
+++ chatinter/plugin_entry.py
@@ -122,7 +122,7 @@ __plugin_meta__ = PluginMetadata(
     """.strip(),
     extra=PluginExtraData(
         author="Copaan & meng-luo",
-        version="1.3.0",
+        version="1.5.0",
         plugin_type=PluginType.DEPENDANT,
         menu_type="其他",
         ignore_prompt=True,
